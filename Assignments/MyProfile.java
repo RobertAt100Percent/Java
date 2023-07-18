b718061d9f8d34fc4e2ce409f4c46869052142db
Robert Daniels Jr <broozerdaniels@yahoo.com>
2023-07-18 13:16:07 -0400

    message

--- Assignments/MyProfile.java
+++ Assignments/MyProfile.java
@@ -7,8 +7,8 @@ public class MyProfile {
                 "Career Goal : Full Stack Engineer\r\n" + //
                 "Age : 22 years old\r\n" + //
                 "An accomplished student with a passion for learning and furthering my knowledge regarding technology. Areas of interest are Web Development, information system analysis/design, and expertise in many coding languages." + //
-                        "I enjoy learning about software engineering and any concepts that encompass this field. I have recently been studying manipulating data structures, front-end development, and connecting relational databases. \r\n" + //
-                        "\r\n" + //
+                "I enjoy learning about software engineering and any concepts that encompass this field. I have recently been studying manipulating data structures, front-end development, and connecting relational databases. \r\n" + //
+                "\r\n" + //
                 "");
     }
 }
