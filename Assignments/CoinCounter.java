916f432869679407eaca91c44b70520d88fba5a7
Robert Daniels Jr <broozerdaniels@yahoo.com>
2023-07-18 14:06:12 -0400

    2nd assignment

---
+++ Assignments/CoinCounter.java
@@ -0,0 +1,31 @@
+package Assignments;
+import java.util.Scanner;
+
+public class CoinCounter {
+public static void main(String[] args){
+    //declare and initalize all needed variables
+    //import and use the scanner object
+    Scanner scan = new Scanner(System.in);
+
+    //prompt the user
+    System.out.println("Welcome To The Year Up Change Counter");
+    System.out.println("This program will take your change and credit your account.");
+
+
+    System.out.println("How many pennies are you inputting : ");
+
+    System.out.println("How many nickles are you inputting : ");
+
+    System.out.println("How many dimes are you inputting : ");
+
+    System.out.println("How many quarters are you inputting : ");
+
+    System.out.println("Thank You");
+
+    System.out.println("we will now credit your account with ______ dollars and ______ cents.");
+    
+
+
+}//end of main class    
+}//end of package
+
