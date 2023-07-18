7d082e1e02e20d4172da5a0bb44ae526dbecbcb3
Robert Daniels Jr <broozerdaniels@yahoo.com>
2023-07-18 15:24:53 -0400

    finished the assignment

--- Assignments/CoinCounter.java
+++ Assignments/CoinCounter.java
@@ -6,23 +6,34 @@ public static void main(String[] args){
     //declare and initalize all needed variables
     //import and use the scanner object
     Scanner scan = new Scanner(System.in);
-
+    double p,n,d,q;
     //prompt the user
-    System.out.println("Welcome To The Year Up Change Counter");
+    System.out.println("**********Welcome To The Year Up Change Counter**********");
     System.out.println("This program will take your change and credit your account.");
 
 
     System.out.println("How many pennies are you inputting : ");
+    p = scan.nextInt();  // Read user input
+    p = p * 0.01;
 
     System.out.println("How many nickles are you inputting : ");
+    n = scan.nextInt();  // Read user input
+    n = n * 0.05;
 
     System.out.println("How many dimes are you inputting : ");
+    d = scan.nextInt();  // Read user input
+    d = d * 0.1;
 
     System.out.println("How many quarters are you inputting : ");
+    q = scan.nextInt();  // Read user input
+    q = q * 0.25;
 
-    System.out.println("Thank You");
+    System.out.println("**********Thank You!!!**********");
+    double sum = p+n+d+q;
+    double cents = sum - Math.floor(sum);
 
-    System.out.println("we will now credit your account with ______ dollars and ______ cents.");
+    System.out.println(sum);
+    System.out.println("we will now credit your account with " + String.format("%.2f", Math.floor(sum))  +" dollars and " + String.format("%.2f", cents) +" cents.");
     
 
 
