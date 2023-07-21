b61bca671c16f1367f5bc0b35ded8bf4b12aa156
Robert Daniels Jr <broozerdaniels@yahoo.com>
2023-07-21 11:21:09 -0400

    sdfghujkl

---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+public class Assignment3 {
+    public static void main(String[] args){
+        /*
+            Scanner class to take in user input
+            .substring(), .toLowerCase(), and optionally the .concat() String methods to handle the username portion.
+            % operator to calculate even / odd number
+            Conditional statements (if-else) for the odd vs even number output and the grade output
+         */
+    
+    
+    
+    }
+}
